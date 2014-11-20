cbb9d7acbf09a0b66ed839a6f6b5a9e257d5f213
lidan <800850@163.com>
2014-11-20 14:17:10 +0800

    修改手机邮箱

---
+++ src/com/sytm/tmkq/ReportDetailModel.java
@@ -0,0 +1,118 @@
+package com.sytm.tmkq;
+
+
+public class ReportDetailModel {
+	private String id = "";
+	private String report = "";
+	private String adddate = "";
+	private String empid = "";
+	private String orgid = "";
+	private String lng = "";
+	private String lat = "";
+	private String address = "";
+	private String province = "";
+	private String city = "";
+	private String district = "";
+	private String attachmentpath="";
+	private String attachmentname="";
+	private String files;
+	private String reply="";
+	private String title="";
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getReport() {
+		return report;
+	}
+	public void setReport(String report) {
+		this.report = report;
+	}
+	public String getAdddate() {
+		return adddate;
+	}
+	public void setAdddate(String adddate) {
+		this.adddate = adddate;
+	}
+	public String getEmpid() {
+		return empid;
+	}
+	public void setEmpid(String empid) {
+		this.empid = empid;
+	}
+	public String getOrgid() {
+		return orgid;
+	}
+	public void setOrgid(String orgid) {
+		this.orgid = orgid;
+	}
+	public String getLng() {
+		return lng;
+	}
+	public void setLng(String lng) {
+		this.lng = lng;
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public String getAttachmentpath() {
+		return attachmentpath;
+	}
+	public void setAttachmentpath(String attachmentpath) {
+		this.attachmentpath = attachmentpath;
+	}
+	public String getAttachmentname() {
+		return attachmentname;
+	}
+	public void setAttachmentname(String attachmentname) {
+		this.attachmentname = attachmentname;
+	}
+	public String getFiles() {
+		return files;
+	}
+	public void setFiles(String files) {
+		this.files = files;
+	}
+	public String getReply() {
+		return reply;
+	}
+	public void setReply(String reply) {
+		this.reply = reply;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+}
